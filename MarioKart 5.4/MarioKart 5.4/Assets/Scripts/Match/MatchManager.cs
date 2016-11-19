424781bc32d8533aae0cf8912670799337e312b0
pablomparada <pablo.paradabol@gmail.com>
2016-11-19 19:31:21 -0200

    - Spawn players at spawn points right in front of start box on road; - Player prefab moved from Prefabs/Lobby to Prefabs; - Change filename GameManager to MatchManager; - Create PlayerManager. Right now it's purpose is to register itself in MatchManager when scene loads.

---
+++ MarioKart 5.4/MarioKart 5.4/Assets/Scripts/Match/MatchManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MatchManager : NetworkBehaviour
+{
+    public static MatchManager instance;
+
+    [SerializeField]
+    private Transform spawnPointGroup;
+
+    private List<Transform> spawnPoints;
+    private List<PlayerManager> players;
+
+    public void Awake()
+    {
+        instance = this;
+        players = new List<PlayerManager>();
+    }
+
+	public void Start()
+	{
+        spawnPoints = GetAllChildrenFromTransform(spawnPointGroup);
+	    UpdatePlayerPositionToSpawnPoints(spawnPoints, players);
+	}
+
+    public void RegisterPlayer(PlayerManager player)
+    {
+        players.Add(player);
+    }
+
+    private void UpdatePlayerPositionToSpawnPoints(List<Transform> spawnPoints, List<PlayerManager> players)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            players[i].transform.position = spawnPoints[i].transform.position;
+        }
+    }
+
+    private List<Transform> GetAllChildrenFromTransform(Transform transform)
+    {
+        List<Transform> transforms = new List<Transform>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transforms.Add(transform.GetChild(i));
+        }
+
+        return transforms;
+    }
+}
---
+++ MarioKart 5.4/MarioKart 5.4/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+
+    public override void OnStartServer()
+    {
+        RegisterInMatch();
+    }
+
+    private void RegisterInMatch()
+    {
+        MatchManager.instance.RegisterPlayer(this);
+    }
+}
