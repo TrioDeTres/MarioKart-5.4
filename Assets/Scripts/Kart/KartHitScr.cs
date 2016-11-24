ce6896ff242013fe8fd2be75b31ea4c043cf1560
naomijub <jnboeira@outlook.com>
2016-11-24 19:40:34 -0300

    tree-kart hit

---
+++ Assets/Scripts/Kart/KartHitScr.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KartHitScr : MonoBehaviour {
+
+	AudioSource hitEffect;
+
+	// Use this for initialization
+	void Start () {
+		hitEffect = GetComponent<AudioSource> ();
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Kart") {
+			hitEffect.loop = false;
+			hitEffect.Play ();
+		}
+	}
+}
