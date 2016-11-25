ae34f3eed87cf3cd0fab47fbb075486956ac7d0d
naomijub <jnboeira@outlook.com>
2016-11-25 19:41:33 -0300

    Audio at char selector

---
+++ Assets/Scripts/CharacterSelect/charSelectorAudio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class charSelectorAudio : MonoBehaviour {
+	private AudioSource[] charAwakeEffect;
+
+	// Use this for initialization
+	void Start () {
+		charAwakeEffect = GetComponents<AudioSource> ();
+		charAwakeEffect [0].Play ();
+
+
+	}
+		
+	void Update(){
+		if (!charAwakeEffect[1].isPlaying) {
+			charAwakeEffect [1].Play ();
+		}
+	}
+
+}
