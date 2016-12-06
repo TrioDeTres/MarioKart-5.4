0dcb1a3adccafd6abedb900068411fff00a3ef56
pablomparada <pablo.paradabol@gmail.com>
2016-12-06 00:14:57 -0200

    - Remove player color icon from lobby scene; - Change interpolation parameters to improve shell rigidbody interpolation; - Change lobby player name input background color  to black.

--- Assets/Scripts/Lobby/LobbyPlayer.cs
+++ Assets/Scripts/Lobby/LobbyPlayer.cs
@@ -7,26 +7,16 @@ using System.Linq;
 
 namespace Prototype.NetworkLobby
 {
-    //Player entry in the lobby. Handle selecting color/setting name & getting ready for the game
-    //Any LobbyHook can then grab it and pass those value to the game player prefab (see the Pong Example in the Samples Scenes)
     public class LobbyPlayer : NetworkLobbyPlayer
     {
-        static Color[] Colors = new Color[] { Color.magenta, Color.red, Color.cyan, Color.blue, Color.green, Color.yellow };
-        //used on server to avoid assigning the same color to two player
-        static List<int> _colorInUse = new List<int>();
-
-        public Button colorButton;
         public InputField nameInput;
         public Button readyButton;
 
         public GameObject localIcone;
         public GameObject remoteIcone;
 
-        //OnMyName function will be invoked on clients when server change the value of playerName
         [SyncVar(hook = "OnMyName")]
         public string playerName = "";
-        [SyncVar(hook = "OnMyColor")]
-        public Color playerColor = Color.white;
 
         public Color OddRowColor = new Color(250.0f / 255.0f, 250.0f / 255.0f, 250.0f / 255.0f, 1.0f);
         public Color EvenRowColor = new Color(180.0f / 255.0f, 180.0f / 255.0f, 180.0f / 255.0f, 1.0f);
@@ -56,7 +46,6 @@ namespace Prototype.NetworkLobby
             }
 
             OnMyName(playerName);
-            OnMyColor(playerColor);
         }
 
         public override void OnStartAuthority()
@@ -98,27 +87,19 @@ namespace Prototype.NetworkLobby
             remoteIcone.gameObject.SetActive(false);
             localIcone.gameObject.SetActive(true);
 
-            if (playerColor == Color.white)
-                CmdColorChange();
-
             ChangeReadyButtonColor(JoinColor);
 
             readyButton.transform.GetChild(0).GetComponent<Text>().text = "READY";
             readyButton.interactable = true;
 
-            //have to use child count of player prefab already setup as "this.slot" is not set yet
             if (playerName == "")
                 CmdNameChanged("Player " + (LobbyPlayerList._instance.playerListContentTransform.childCount-1));
 
-            //we switch from simple name display to name input
-            colorButton.interactable = true;
             nameInput.interactable = true;
 
             nameInput.onEndEdit.RemoveAllListeners();
             nameInput.onEndEdit.AddListener(OnNameChanged);
 
-            colorButton.onClick.RemoveAllListeners();
-            colorButton.onClick.AddListener(OnColorClicked);
 
             readyButton.onClick.RemoveAllListeners();
             readyButton.onClick.AddListener(OnReadyClicked);
@@ -135,7 +116,6 @@ namespace Prototype.NetworkLobby
                 textComponent.color = Color.white;      
                 ChangeReadyButtonColor(ReadyColor);
                 readyButton.interactable = false;
-                colorButton.interactable = false;
                 nameInput.interactable = false;
             }
             else
@@ -146,7 +126,6 @@ namespace Prototype.NetworkLobby
                 textComponent.text = isLocalPlayer ? "READY" : "...";
                 textComponent.color = Color.white;
                 readyButton.interactable = isLocalPlayer;
-                colorButton.interactable = isLocalPlayer;
                 nameInput.interactable = isLocalPlayer;
             }
 
@@ -155,32 +134,10 @@ namespace Prototype.NetworkLobby
             readyButton.colors = readyButtonCollorBlock;
         }
 
-        public void OnPlayerListChanged(int idx)
-        { 
-            GetComponent<Image>().color = (idx % 2 == 0) ? EvenRowColor : OddRowColor;
-        }
-
-        ///===== callback from sync var
-
         public void OnMyName(string newName)
         {
             playerName = newName;
             nameInput.text = playerName;
-        }
-
-        public void OnMyColor(Color newColor)
-        {
-            playerColor = newColor;
-            colorButton.GetComponent<Image>().color = newColor;
-        }
-
-        //===== UI Handler
-
-        //Note that those handler use Command function, as we need to change the value on the server not locally
-        //so that all client get the new value throught syncvar
-        public void OnColorClicked()
-        {
-            CmdColorChange();
         }
 
         public void OnReadyClicked()
@@ -206,45 +163,6 @@ namespace Prototype.NetworkLobby
         }
 
         [Command]
-        public void CmdColorChange()
-        {
-            int idx = System.Array.IndexOf(Colors, playerColor);
-
-            int inUseIdx = _colorInUse.IndexOf(idx);
-
-            if (idx < 0) idx = 0;
-
-            idx = (idx + 1) % Colors.Length;
-
-            bool alreadyInUse = false;
-
-            do
-            {
-                alreadyInUse = false;
-                for (int i = 0; i < _colorInUse.Count; ++i)
-                {
-                    if (_colorInUse[i] == idx)
-                    {//that color is already in use
-                        alreadyInUse = true;
-                        idx = (idx + 1) % Colors.Length;
-                    }
-                }
-            }
-            while (alreadyInUse);
-
-            if (inUseIdx >= 0)
-            {//if we already add an entry in the colorTabs, we change it
-                _colorInUse[inUseIdx] = idx;
-            }
-            else
-            {//else we add it
-                _colorInUse.Add(idx);
-            }
-
-            playerColor = Colors[idx];
-        }
-
-        [Command]
         public void CmdNameChanged(string name)
         {
             playerName = name;
@@ -263,25 +181,10 @@ namespace Prototype.NetworkLobby
             ChatPanel.singleton.CreateMessage(message, playerNickname);
         }
 
-        //Cleanup thing when get destroy (which happen when client kick or disconnect)
         public void OnDestroy()
         {
             LobbyPlayerList._instance.RemovePlayer(this);
             if (LobbyManager.singleton != null) LobbyManager.singleton.OnPlayersNumberModified(-1);
-
-            int idx = System.Array.IndexOf(Colors, playerColor);
-
-            if (idx < 0)
-                return;
-
-            for (int i = 0; i < _colorInUse.Count; ++i)
-            {
-                if (_colorInUse[i] == idx)
-                {//that color is already in use
-                    _colorInUse.RemoveAt(i);
-                    break;
-                }
-            }
         }
     }
 }
--- Assets/Scripts/Lobby/LobbyPlayerList.cs
+++ Assets/Scripts/Lobby/LobbyPlayerList.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 
 namespace Prototype.NetworkLobby
 {
-    //List of players in the lobby
     public class LobbyPlayerList : MonoBehaviour
     {
         public static LobbyPlayerList _instance = null;
@@ -32,11 +31,9 @@ namespace Prototype.NetworkLobby
 
         void Update()
         {
-            //this dirty the layout to force it to recompute evryframe (a sync problem between client/server
-            //sometime to child being assigned before layout was enabled/init, leading to broken layouting)
-            
-            if(_layout)
+            if(_layout) {
                 _layout.childAlignment = Time.frameCount%2 == 0 ? TextAnchor.UpperCenter : TextAnchor.UpperLeft;
+            }
         }
 
         public void AddPlayer(LobbyPlayer player)
@@ -48,24 +45,11 @@ namespace Prototype.NetworkLobby
 
             player.transform.SetParent(playerListContentTransform, false);
             addButtonRow.transform.SetAsLastSibling();
-
-            PlayerListModified();
         }
 
         public void RemovePlayer(LobbyPlayer player)
         {
             _players.Remove(player);
-            PlayerListModified();
-        }
-
-        public void PlayerListModified()
-        {
-            int i = 0;
-            foreach (LobbyPlayer p in _players)
-            {
-                p.OnPlayerListChanged(i);
-                ++i;
-            }
         }
 
         public int NumberOfPlayers()
